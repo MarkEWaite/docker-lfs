f3417f8fdc01074684a87a55f2b7651cf164f332
Mark Waite <mark.earl.waite@gmail.com>
2017-02-18 07:59:06 -0700

    Allow different username and hostname to run image
    Replace constants in docker image at runtime

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,11 @@
 #! /bin/bash -e
 
 : "${JENKINS_HOME:="/var/jenkins_home"}"
+
+: ${JENKINS_HOSTNAME:=localhost}
+bash -c ". /usr/local/bin/jenkins-support; replace_constants "JENKINS_HOSTNAME" $JENKINS_HOSTNAME"
+bash -c ". /usr/local/bin/jenkins-support; replace_constants "LOGNAME" $LOGNAME"
+
 touch "${COPY_REFERENCE_FILE_LOG}" || { echo "Can not write to ${COPY_REFERENCE_FILE_LOG}. Wrong volume permissions?"; exit 1; }
 echo "--- Copying files at $(date)" >> "$COPY_REFERENCE_FILE_LOG"
 find /usr/share/jenkins/ref/ -type f -exec bash -c '. /usr/local/bin/jenkins-support; for arg; do copy_reference_file "$arg"; done' _ {} +
@@ -19,7 +24,8 @@ if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
     jenkins_opts_array+=( "$item" )
   done < <([[ $JENKINS_OPTS ]] && xargs printf '%s\0' <<<"$JENKINS_OPTS")
 
-  exec java "${java_opts_array[@]}" -jar /usr/share/jenkins/jenkins.war "${jenkins_opts_array[@]}" "$@"
+  echo java  ${java_opts_array[@]}  -jar /usr/share/jenkins/jenkins.war  ${jenkins_opts_array[@]}   $@
+  exec java  ${java_opts_array[@]}  -jar /usr/share/jenkins/jenkins.war  ${jenkins_opts_array[@]}   $@
 fi
 
 # As argument is not jenkins, assume user want to run his own process, for example a `bash` shell to explore this image
