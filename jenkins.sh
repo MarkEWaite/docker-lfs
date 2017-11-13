53029f0948a1c0a0501e24daa9ab8f3519e98982
Mark Waite <mark.earl.waite@gmail.com>
2017-11-13 09:56:10 -0700

    Remove call to replace_constants
    Removes differences from upstream/master

--- jenkins.sh
+++ jenkins.sh
@@ -1,12 +1,6 @@
 #! /bin/bash -e
 
 : "${JENKINS_HOME:="/var/jenkins_home"}"
-
-: "${JENKINS_HOSTNAME:=localhost}"
-bash -c ". /usr/local/bin/jenkins-support; replace_constants JENKINS_HOSTNAME $JENKINS_HOSTNAME"
-bash -c ". /usr/local/bin/jenkins-support; replace_constants LOGNAME $LOGNAME"
-mkdir -p "$JENKINS_HOME/.jenkins/cache/jars"
-
 touch "${COPY_REFERENCE_FILE_LOG}" || { echo "Can not write to ${COPY_REFERENCE_FILE_LOG}. Wrong volume permissions?"; exit 1; }
 echo "--- Copying files at $(date)" >> "$COPY_REFERENCE_FILE_LOG"
 find /usr/share/jenkins/ref/ \( -type f -o -type l \) -exec bash -c '. /usr/local/bin/jenkins-support; for arg; do copy_reference_file "$arg"; done' _ {} +
