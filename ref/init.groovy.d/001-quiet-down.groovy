f92f350f91b18c87e56601d74c9b30e011c4cce7
Mark Waite <mark.earl.waite@gmail.com>
2019-10-05 06:13:18 -0600

    Don't care about case of START_QUIET
    True and true both enable quiet startup

--- ref/init.groovy.d/001-quiet-down.groovy
+++ ref/init.groovy.d/001-quiet-down.groovy
@@ -1,4 +1,4 @@
 // Enter "Preparing to shutdown" state
-if (System.getenv("START_QUIET") == "True") {
+if (System.getenv("START_QUIET").equalsIgnoreCase('true')) {
   jenkins.model.Jenkins.instance.doQuietDown()
 }
