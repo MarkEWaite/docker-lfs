ea4b17a59fc983186fa5a8e6d3468be9b8bb4187
Mark Waite <mark.earl.waite@gmail.com>
2019-12-14 15:23:07 -0700

    Test git status output differently

--- docker_build.py
+++ docker_build.py
@@ -170,8 +170,8 @@ def merge_predecessor_branch(current_branch, all_branches):
 #-----------------------------------------------------------------------
 
 def push_current_branch():
-    status_output = subprocess.check_output([ "git", "status", "--porcelain=1"]).strip()
-    if not status_output:
+    status_output = subprocess.check_output([ "git", "status"]).strip()
+    if "Your branch is ahead of " in status_output:
 	command = [ "git", "push" ]
 	print("Pushing current branch")
 	subprocess.check_call(command)
