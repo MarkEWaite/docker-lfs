734bf2ad0b646305e04349f0abadaf105db2d2d9
Mark Waite <mark.earl.waite@gmail.com>
2021-05-29 10:38:22 -0600

    Output the download command when updates detected

--- docker_build.py
+++ docker_build.py
@@ -132,10 +132,13 @@ def undo_replace_constants_in_ref():
 def update_plugins(base_jenkins_version):
     if not os.path.isdir("ref"):
         return
-    command = [ "./jenkins-plugin-cli.sh", "--jenkins-version", base_jenkins_version, "--plugin-download-directory", "ref/plugins", "--plugin-file", "plugins.txt", "--available-updates"]
-    update_plugins_output = subprocess.check_output(command).strip().decode("utf-8")
+    available_updates_command = [ "./jenkins-plugin-cli.sh", "--jenkins-version", base_jenkins_version, "--plugin-download-directory", "ref/plugins", "--plugin-file", "plugins.txt", "--available-updates"]
+    update_plugins_output = subprocess.check_output(available_updates_command).strip().decode("utf-8")
     if "has an available update" in update_plugins_output:
-        print("Stopping because plugin updates are available: " + update_plugins_output)
+        print("Plugin update available")
+        print("Stopping because a plugin update is available: " + update_plugins_output)
+        download_updates_command = [ "./jenkins-plugin-cli.sh", "--jenkins-version", base_jenkins_version, "--plugin-download-directory", "ref/plugins", "--plugin-file", "plugins.txt"]
+        print("Run " + download_updates_command.join(" "))
         quit()
 
 #-----------------------------------------------------------------------
