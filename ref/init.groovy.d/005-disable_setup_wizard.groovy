cf96632790e4e70c64616167780ca91766cd05ae
Mark Waite <mark.earl.waite@gmail.com>
2026-03-28 12:39:25 -0600

    Do not run the setup wizard (groovy configuration)

--- ref/init.groovy.d/005-disable_setup_wizard.groovy
+++ ref/init.groovy.d/005-disable_setup_wizard.groovy
@@ -1,3 +1,5 @@
-// Do not run the setup wizard (also requires command line argument)
+import jenkins.install.InstallState
+import jenkins.model.Jenkins
 
-jenkins.instance.setInstallState(jenkins.install.InstallState.INITIAL_SETUP_COMPLETED)
+// Also requires command line argument at startup (see README)
+Jenkins.getInstance().setInstallState(InstallState.INITIAL_SETUP_COMPLETED)
